a24b6fd716b7a00c0affb8a331c247ed1db74822
cherepanov <andrejcerepanov7@gmail.com>
2026-07-17 19:42:56 +0700

    3

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ function getDefaultData() {
         // 2. ГАЛЕРЕЯ УСАДЬБЫ (вставьте ссылки на фото)
         // --------------------------------------------------------
         gallery: [
-            'https://i.ibb.co/7J6gjFCn/dg8-IHi-TNf96-TBq-H4-JY1-LQp-MWf-V1-G3-WTf-Gnqj-KT13fup2-S2-GKd-E2b8-Oz-VW17-Rp-VOtv-Sw-Cm-DU83n-PQ.jpg',
-            'https://i.ibb.co/Mw1r2hc/Om-Mg-GVb-Aj18j-Pi-OCSA5-JCJn8e-Y6i9-Vo0m-Acd-SLFAy6-SXg-Ph-ETy-J9a8-Eufs-H3-W7bq-DW2-Ahrm-Sg-Ug.jpg',
-            'https://i.ibb.co/LzMvR13T/fm-RRr-TUSVh-ZAN1d-DI4-Acw-Nl7qig-MFUJb-AGJt-Vt-EW7-WY1-Jr7-Eu5-Ox-Sn-YDh-Rk-ZZlt1-HIXOVLh-A7-Q.jpg',
-	    'https://i.ibb.co/7xqm8CtT/z-Pq-EZ6l-D5-IQq-AZPYAq-Yj-LGv3-HGCes-OVF4l-Tn-Vnpy3r-SXOWPUn-Q7c5g-Ij-Rk-UE57-WFNg-HRkz-H8d-A.jpg',
-            'https://i.ibb.co/JRV2rSWn/mj-JP4-KFAXOVUDl0-Rs-Xb-Pw5bcl-QTlk-Eh-XHWB9-El-HKYGkwbh1-FOYh-Jy-BSu-Jh-O7-UBUYVn-Tn6-Ls3-Osw.jpg',
-	    'https://i.ibb.co/4gn88qhK/F4-JQn-TL6-Wd-Srz-KE5sz-QGf-DXj-REGJt-JFua-U6f-Bwhv-A0ew-K5l-HYr-Y0j9d-Qj-Tx5-HDM2-IAd-Vd8m-Wg.jpg',
-	    'https://i.ibb.co/Ps1YzbM7/Mmp-Ax-V0-VFp-GWu-KM2x-Iz0-OBk-Ago-WB2s-Fe-Ch-Wt-BPz-Wl-YO2qh-CBQe-WRb-Vb1-Db-X0-VRkh-FTr-Ye-Vb-Ww.jpg',
-            'https://i.ibb.co/7xttWVBH/ir-Fa-NRw-Gsq-V7-EFr4o2-Hr-BDkn-FHIMk-JC1-UFz2-Ag-Wq-Lr-J9-JN0j5-KMv-Dy7n-R5s-Hr8g-LZ1w9-JP3-VQ.jpg',
-	    'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg',
-	    'https://i.ibb.co/Rp0Dfd8P/ul5h4-Mu-DIAs-Bd-CUXigc-G1e6p-TQZaw-Kq-Pp-Ws-HEWr-EUc7-Ch-Cr-ULztn-S6rm2-VWEI7h7s5-Ac-ZN9p-Gn-XA.jpg'	
-        ],
+    'https://i.ibb.co/7J6gjFCn/dg8-IHi-TNf96-TBq-H4-JY1-LQp-MWf-V1-G3-WTf-Gnqj-KT13fup2-S2-GKd-E2b8-Oz-VW17-Rp-VOtv-Sw-Cm-DU83n-PQ.jpg',
+    'https://i.ibb.co/Mw1r2hc/Om-Mg-GVb-Aj18j-Pi-OCSA5-JCJn8e-Y6i9-Vo0m-Acd-SLFAy6-SXg-Ph-ETy-J9a8-Eufs-H3-W7bq-DW2-Ahrm-Sg-Ug.jpg',
+    'https://i.ibb.co/LzMvR13T/fm-RRr-TUSVh-ZAN1d-DI4-Acw-Nl7qig-MFUJb-AGJt-Vt-EW7-WY1-Jr7-Eu5-Ox-Sn-YDh-Rk-ZZlt1-HIXOVLh-A7-Q.jpg',
+    'https://i.ibb.co/7xqm8CtT/z-Pq-EZ6l-D5-IQq-AZPYAq-Yj-LGv3-HGCes-OVF4l-Tn-Vnpy3r-SXOWPUn-Q7c5g-Ij-Rk-UE57-WFNg-HRkz-H8d-A.jpg',
+    'https://i.ibb.co/JRV2rSWn/mj-JP4-KFAXOVUDl0-Rs-Xb-Pw5bcl-QTlk-Eh-XHWB9-El-HKYGkwbh1-FOYh-Jy-BSu-Jh-O7-UBUYVn-Tn6-Ls3-Osw.jpg',
+    'https://i.ibb.co/4gn88qhK/F4-JQn-TL6-Wd-Srz-KE5sz-QGf-DXj-REGJt-JFua-U6f-Bwhv-A0ew-K5l-HYr-Y0j9d-Qj-Tx5-HDM2-IAd-Vd8m-Wg.jpg',
+    'https://i.ibb.co/Ps1YzbM7/Mmp-Ax-V0-VFp-GWu-KM2x-Iz0-OBk-Ago-WB2s-Fe-Ch-Wt-BPz-Wl-YO2qh-CBQe-WRb-Vb1-Db-X0-VRkh-FTr-Ye-Vb-Ww.jpg',
+    'https://i.ibb.co/7xttWVBH/ir-Fa-NRw-Gsq-V7-EFr4o2-Hr-BDkn-FHIMk-JC1-UFz2-Ag-Wq-Lr-J9-JN0j5-KMv-Dy7n-R5s-Hr8g-LZ1w9-JP3-VQ.jpg',
+    'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg',
+    'https://i.ibb.co/Rp0Dfd8P/ul5h4-Mu-DIAs-Bd-CUXigc-G1e6p-TQZaw-Kq-Pp-Ws-HEWr-EUc7-Ch-Cr-ULztn-S6rm2-VWEI7h7s5-Ac-ZN9p-Gn-XA.jpg'
+],
 
         // --------------------------------------------------------
         // 3. НОМЕРА (меняйте цены, описания, ссылки на фото)
@@ -40,15 +40,12 @@ function getDefaultData() {
                 price: '7 000 ₽',
                 image: 'https://i.ibb.co/HfdDySWV/x-TM7-IQN8v-d-A33u-CX-LMo-Ydt4-Mp-LHx-Thz-Xy1-6a-OKf-YIw-Hw-Rhcr3-Ie-SXAWUt-BHVSn-RDYXf9t3-N5-Qa25-Rvb-Ibz.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-	        	'https://i.ibb.co/6RLqFXz0/Oi9Lw-NZu8-D-WHUrqn-V-se2rosp-ZOPRznr6r-RGCisna-Qpf8-XSJBT5-IEz-CBMGHPQKV6r9-I6-KFrov-Us-GLa-UVn-Ljip-Q.jpg',
-			'https://i.ibb.co/Sz1xq9m/9e-BDen56w2-Kt0-HL0-Tmb2t-O6-c-SGpx-A2-F1-Bujd5g2run-Pd-GEmk-YO6s-J855j5-Vgg6-LHHNTu-Rtu-b-Bcw64-rgw6w01r.jpg',
-			'https://i.ibb.co/VYhYsMbZ/Casxj-TSE6ak-BUEu-z-H-OKGVVwshv-FHxht-It9-CEzbu-o-Exibe-YSc-Bq5g-K-h-DUBX1enw-HW2-7b2ohrz2ll-WHiu6-H.jpg',
-			'https://i.ibb.co/kZWr1C2/HX3w-1-CNwi-Yj-Ak-DDVBUN5-OO19o-GVn-Gr-BLLIWrvudrh-Ekw-TCub-Jg-Ta-C18-XFwok-Wm-Ik0pnb-OBQi1-cx5-Lj-Zl-O.jpg',
-			'https://i.ibb.co/LDY6nqJ6/NAu2-Wokedm6-Ge6jxr-SLMP2-CZ5d-Qlw-B8-EZ3-C95-JOL-14-Faog-Br-Bz6t29-Ws-X6-SNWbg-AW2w-MHx-Z21c-Eyf-Rc-MKDj-Oik.jpg',
-			'https://i.ibb.co/k63DRK00/oxjk-Bx2-Z2t-PNj5kn-IHCgg-Lqf4-C1y-Uw-ALJ3ngo-Wax28-K3-A5-C7-S3-PWtg7-Bh-Rg1-LPa-N8hh7r-I3bz-Mr-YPc4-Kup-XPs8-J.jpg',
-			'https://i.ibb.co/nqFKQxNF/z-BWwrei-IDVw-B0w9-ZZ78d-YFz-QUid-TUx-Ikfjk-A-Ut4-Lyl-Tv-JJEMZl-Fzc-OX5-Xq-Qjmr-KWx-Un-Fsl8g-V1l-SIhjy.jpg'
-                ],
-                description: 'Уютный номер для двоих. Всё необходимое для комфортного отдыха: двуспальная кровать,бесплатный Wi-Fi, санузел с душем.'
+    'https://i.ibb.co/6RLqFXz0/Oi9Lw-NZu8-D-WHUrqn-V-se2rosp-ZOPRznr6r-RGCisna-Qpf8-XSJBT5-IEz-CBMGHPQKV6r9-I6-KFrov-Us-GLa-UVn-Ljip-Q.jpg',
+    'https://i.ibb.co/nqFKQxNF/z-BWwrei-IDVw-B0w9-ZZ78d-YFz-QUid-TUx-Ikfjk-A-Ut4-Lyl-Tv-JJEMZl-Fzc-OX5-Xq-Qjmr-KWx-Un-Fsl8g-V1l-SIhjy.jpg',
+    'https://i.ibb.co/Df8W8HH5/uj-Kp-MUmg2lwm-Xz-S-Gu-N6-YTgc-Gx0l-BBMq-Cvy93-BDyc-OZ9c4uzfd-ANV-ynyf-Dj-GSUFwr20u2-CFu-Qo-BM9-Msrs-Ry.jpg',
+    'https://i.ibb.co/Ldgq25Rc/3-C-g3fnn7nj-LKVUf-Vn-Baz-Avi-TYLNL4ll2z-G9jdk4b-Puwed-J3-ETL0-I6-Je-A5-Kx-JQy6mbr-OGy-Iw3qxrs-Xnn9c-NXPR.jpg'
+],
+                description: 'Уютный номер для двоих. Всё необходимое для комфортного отдыха: двуспальная кровать, бесплатный Wi-Fi, санузел с душем.'
             },
             {
                 id: 2,
@@ -56,15 +53,14 @@ function getDefaultData() {
                 price: '7 000 ₽',
                 image: 'https://i.ibb.co/DgKDSWT9/mndm-Uh-OUOvl0-R1vkt-XPJ3g-C8-Bh-Ega-Gi-S8-Jn-TKJt-D87-YU1-Mj-Xf-FMZa-Udy2g-W0-Re3b-ORLpr-ALNcg.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-			'https://i.ibb.co/21MrhPmH/ZRco-KJQygz-Hm-RZEAjd-THBOivjszq9bd-AVgg-Ng-E53-ZD1-LKb-JS6-Mnl-M3-Fe-Kh-WMv-Xpu8jq-K13-F2z-RYQ.jpg',
-			'https://i.ibb.co/Y71gbhQ8/Hm-GI5bccew-MUKc7-Up-BKqj1-S3-Fhljs7c-VVVMn-Fub-DVk-XI8-WZ3wz-Vi7j6h-Ly-MEGCy-Ng-YAr-H6p-WP91-Cg.jpg',
-			'https://i.ibb.co/GrpYSj3/u-OWG9-O0-YAk3-D4w-Yc-CRNFMk4-Fo6wq-X1-Jn-NNMR8y-UShx-F4-NSb-L9f3-E6n-Fpx7-XB78-RKJ-W6pz-DCh-EGa-A.jpg',
-			'https://i.ibb.co/5xMGNdkr/A9-KNEOk-Rg3-Ks6-JARj-Vp-R8-W2-JVPM1h-CV6-EOzx-Y4dt-Fn-Ha-X3rc1-SDq-FZTVSj-T1-Ri-Jt-POHp-XCPy-gg.jpg',
-			'https://i.ibb.co/4n2vjWWq/2-IWe-XKvl-HEGBj-UDR8-GGcbd-41hwfn-Ap-J8-Deam-CLXn-Is-NAOZVx-Lun-TDDRDcx-K7-NVJm-GYp-FJYx-VTg-A.jpg',
-			'https://i.ibb.co/gMLg5MLv/AQj2po-AS0sbkyb-Fh-My-Y7j-Fp-FI69n-Jf-PPj-Ee7-MZb-Fu1-D3m-Zh-O6-Tj-OOz2-Qso-K1-TJ8-Ej-Smp-JDTR8d-A.jpg',
-			'https://i.ibb.co/x8C02zQV/4-MBw-UVps-YEfv-P1-IPBse-TXq-Dl-Z5gr-FKh-YHWHj-Qy-YNg-H0-IWEa-U2-PI0n-IFBQNi-Pb-Yi-ZORBj-Kp-Wn-A.jpg',
-			'https://i.ibb.co/1fyNBfvC/6s-FR8-Mg-ISLdlz-F6vvt9-YOHj-Kh-Eg-S4-LVj-EOQJvs-EVQz-Sd-DNIn-Hs8-U6l-Su-Cuc-CYg-AQpo6s-Cf-W0-IJ5j-A.jpg'
-		],
+    'https://i.ibb.co/21MrhPmH/ZRco-KJQygz-Hm-RZEAjd-THBOivjszq9bd-AVgg-Ng-E53-ZD1-LKb-JS6-Mnl-M3-Fe-Kh-WMv-Xpu8jq-K13-F2z-RYQ.jpg',
+    'https://i.ibb.co/Y71gbhQ8/Hm-GI5bccew-MUKc7-Up-BKqj1-S3-Fhljs7c-VVVMn-Fub-DVk-XI8-WZ3wz-Vi7j6h-Ly-MEGCy-Ng-YAr-H6p-WP91-Cg.jpg',
+    'https://i.ibb.co/GrpYSj3/u-OWG9-O0-YAk3-D4w-Yc-CRNFMk4-Fo6wq-X1-Jn-NNMR8y-UShx-F4-NSb-L9f3-E6n-Fpx7-XB78-RKJ-W6pz-DCh-EGa-A.jpg',
+    'https://i.ibb.co/5xMGNdkr/A9-KNEOk-Rg3-Ks6-JARj-Vp-R8-W2-JVPM1h-CV6-EOzx-Y4dt-Fn-Ha-X3rc1-SDq-FZTVSj-T1-Ri-Jt-POHp-XCPy-gg.jpg',
+    'https://i.ibb.co/4n2vjWWq/2-IWe-XKvl-HEGBj-UDR8-GGcbd-41hwfn-Ap-J8-Deam-CLXn-Is-NAOZVx-Lun-TDDRDcx-K7-NVJm-GYp-FJYx-VTg-A.jpg',
+    'https://i.ibb.co/gMLg5MLv/AQj2po-AS0sbkyb-Fh-My-Y7j-Fp-FI69n-Jf-PPj-Ee7-MZb-Fu1-D3m-Zh-O6-Tj-OOz2-Qso-K1-TJ8-Ej-Smp-JDTR8d-A.jpg',
+    'https://i.ibb.co/x8C02zQV/4-MBw-UVps-YEfv-P1-IPBse-TXq-Dl-Z5gr-FKh-YHWHj-Qy-YNg-H0-IWEa-U2-PI0n-IFBQNi-Pb-Yi-ZORBj-Kp-Wn-A.jpg'
+],
                 description: 'Просторная студия для компании до 4 человек. Двуспальная кровать + диван-кровать. Есть мини-кухня с холодильником и чайником.'
             },
             {
@@ -73,35 +69,36 @@ function getDefaultData() {
                 price: '8 000 ₽',
                 image: 'https://i.ibb.co/v6M9V1rN/v-Vi-Kg-Fmz-LKw-EJhs-HLlb-Cd7b-V1ph-B4to-Ip-KDhz2vsnzk-Y0-Bdh-MN42-YUx-Yx7x1n-IEZWYt-Qk2-M3a-Q.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-			'https://i.ibb.co/QvL8QQn7/5h-FS3-Bj-OL1yzwq-IGWxi-Yh6-At-Eu64-Kg-SE4m8-JOAl-OVSPs-VF4-Lg-KZRj-NKSu-Os-CA9-D5-S1-K9x4m-LCQ.jpg',
-			'https://i.ibb.co/nMMwfzyk/SMLBd-Qt-UO5-IK1pr-Tu-T3ri-Au-FFXLt-E1aa-DWZy-Vze7ewe-Snkw-S0f7k-SKb-Umth-NSl-Cvy4s-HF4-KREk5l9n-Zw.jpg',
-			'https://i.ibb.co/9mh2qdgs/BYl-MVJqs0n-Y15-QJw-Fu9-UGTKt-Eq-Cw3lz-Ts-K4-Hv-Ypoa14ov-SU5f-Iua-JS-YSb4b-OAJTj-v1os-FAr-CQr-Cy-DA.jpg',
-			'https://i.ibb.co/PvS3KntW/FEGQn-X4-F0so9f4k-UY9-G4-El-Ck6u-A49r-WLx-Gt7-CRm-Bf-FLR-V4yeq-FZwj-VH0-FLBCz-OWdt1-Xb22a-LEw.jpg',
-			'https://i.ibb.co/d4KSnHyN/z-Eao5-Kx-CGhz-Ric-TGy-KJm-OGMPOvz-KIg8-QXJCNhzw1q-Vi-Qh-I06-M4-Nj-NWck-PUVb-HVTK3-KYgba-Zc-Pw.jpg',
-			'https://i.ibb.co/1fyNBfvC/6s-FR8-Mg-ISLdlz-F6vvt9-YOHj-Kh-Eg-S4-LVj-EOQJvs-EVQz-Sd-DNIn-Hs8-U6l-Su-Cuc-CYg-AQpo6s-Cf-W0-IJ5j-A.jpg'
-		],
+    'https://i.ibb.co/QvL8QQn7/5h-FS3-Bj-OL1yzwq-IGWxi-Yh6-At-Eu64-Kg-SE4m8-JOAl-OVSPs-VF4-Lg-KZRj-NKSu-Os-CA9-D5-S1-K9x4m-LCQ.jpg',
+    'https://i.ibb.co/nMMwfzyk/SMLBd-Qt-UO5-IK1pr-Tu-T3ri-Au-FFXLt-E1aa-DWZy-Vze7ewe-Snkw-S0f7k-SKb-Umth-NSl-Cvy4s-HF4-KREk5l9n-Zw.jpg',
+    'https://i.ibb.co/9mh2qdgs/BYl-MVJqs0n-Y15-QJw-Fu9-UGTKt-Eq-Cw3lz-Ts-K4-Hv-Ypoa14ov-SU5f-Iua-JS-YSb4b-OAJTj-v1os-FAr-CQr-Cy-DA.jpg',
+    'https://i.ibb.co/PvS3KntW/FEGQn-X4-F0so9f4k-UY9-G4-El-Ck6u-A49r-WLx-Gt7-CRm-Bf-FLR-V4yeq-FZwj-VH0-FLBCz-OWdt1-Xb22a-LEw.jpg',
+    'https://i.ibb.co/d4KSnHyN/z-Eao5-Kx-CGhz-Ric-TGy-KJm-OGMPOvz-KIg8-QXJCNhzw1q-Vi-Qh-I06-M4-Nj-NWck-PUVb-HVTK3-KYgba-Zc-Pw.jpg',
+    'https://i.ibb.co/1fyNBfvC/6s-FR8-Mg-ISLdlz-F6vvt9-YOHj-Kh-Eg-S4-LVj-EOQJvs-EVQz-Sd-DNIn-Hs8-U6l-Su-Cuc-CYg-AQpo6s-Cf-W0-IJ5j-A.jpg'
+],
                 description: 'Дом для большой компании. Две отдельные комнаты, две двуспальные кровати + раскладное кресло + односпальная кровать. Полностью оборудованная кухня.'
             },
             {
-                id: 4,
-                name: 'Семейный неблагоустроенный номер летний (3 взрослых + 2 детей)',
-                price: '3 800 ₽',
-                image: 'https://i.ibb.co/KzWN06nR/E8m-Bgd-ZWp-F4-OEW8x-FNd36x6h-AHSNw-UFE3-HNy2d2v-QQyb3-Ay-Yy6-E8uj-BCbr-KSLOp-VNQ2ea-Sg-Gg.jpg', // ← СЮДА ССЫЛКУ
-                gallery: [
-			'https://i.ibb.co/Df8W8HH5/uj-Kp-MUmg2lwm-Xz-S-Gu-N6-YTgc-Gx0l-BBMq-Cvy93-BDyc-OZ9c4uzfd-ANV-ynyf-Dj-GSUFwr20u2-CFu-Qo-BM9-Msrs-Ry.jpg',
-			'https://i.ibb.co/Ldgq25Rc/3-C-g3fnn7nj-LKVUf-Vn-Baz-Avi-TYLNL4ll2z-G9jdk4b-Puwed-J3-ETL0-I6-Je-A5-Kx-JQy6mbr-OGy-Iw3qxrs-Xnn9c-NXPR.jpg',
-			'https://i.ibb.co/vx86PFrt/q-Guy-Oo3-ONr0xw-Jy-IB3eetj-SBrc-Bg-L6-Fs-Fo-Gl-Vh9w-Fvj-Ov-Ac-Jf-MA4o-Jf0-Fnl3-Uqdx1-Rr5h-HWWd-A.jpg'
-		],
-                description: 'Просторный летний номер для семьи с детьми. 3 обычные кровати + двухъярусная кровать (до 160 см рост). Душ и туалет на территории, кухонная зона в беседке.'
-            },
+                {
+    id: 4,
+    name: 'Семейный неблагоустроенный номер летний (3 взрослых + 2 детей)',
+    price: '4 200 ₽',
+    image: 'https://i.ibb.co/KzWN06nR/E8m-Bgd-ZWp-F4-OEW8x-FNd36x6h-AHSNw-UFE3-HNy2d2v-QQyb3-Ay-Yy6-E8uj-BCbr-KSLOp-VNQ2ea-Sg-Gg.jpg',
+    gallery: [
+        'https://i.ibb.co/Df8W8HH5/uj-Kp-MUmg2lwm-Xz-S-Gu-N6-YTgc-Gx0l-BBMq-Cvy93-BDyc-OZ9c4uzfd-ANV-ynyf-Dj-GSUFwr20u2-CFu-Qo-BM9-Msrs-Ry.jpg',
+        'https://i.ibb.co/Ldgq25Rc/3-C-g3fnn7nj-LKVUf-Vn-Baz-Avi-TYLNL4ll2z-G9jdk4b-Puwed-J3-ETL0-I6-Je-A5-Kx-JQy6mbr-OGy-Iw3qxrs-Xnn9c-NXPR.jpg',
+        'https://i.ibb.co/vx86PFrt/q-Guy-Oo3-ONr0xw-Jy-IB3eetj-SBrc-Bg-L6-Fs-Fo-Gl-Vh9w-Fvj-Ov-Ac-Jf-MA4o-Jf0-Fnl3-Uqdx1-Rr5h-HWWd-A.jpg'
+    ],
+    description: 'Просторный летний номер для семьи с детьми. 3 обычные кровати + двухъярусная кровать (до 160 см рост). Душ и туалет на территории, кухонная зона в беседке. Отличный вариант для семейного отдыха на природе.'
+},
             {
                 id: 5,
                 name: 'Трехместный летний неблагоустроенный номер',
                 price: '3 600 ₽',
                 image: 'https://i.ibb.co/HTHG46SY/OHViy-FYd-Vet-L1-EHt-WCPVw-Wn-YGZbax-WZx-DBc7w-Gq-Oi1cz-Rte-IQj-Tx-Fsl-BDiiu-Di-HEm7-Yn5-Ml8-Wl-Dw.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-			'https://i.ibb.co/TzJsbJK/4k-Gw-TGXy-RWXW1r-Uq-WJLA86c-Bg5u-Js-Wc-R8p-Se1-HG1q-GM5-Elih4-P3to-XMt-Ljwt-ZZf-Px-SWuonq-Tw5-A.jpg'
-		],
+    'https://i.ibb.co/TzJsbJK/4k-Gw-TGXy-RWXW1r-Uq-WJLA86c-Bg5u-Js-Wc-R8p-Se1-HG1q-GM5-Elih4-P3to-XMt-Ljwt-ZZf-Px-SWuonq-Tw5-A.jpg'
+],
                 description: 'Уютный летний номер для троих. Двуспальная и односпальная кровать. В номере: стол, раковина. Душ и туалет на территории, кухонная зона в беседке.'
             },
             {
@@ -110,8 +107,9 @@ function getDefaultData() {
                 price: '2 400 ₽',
                 image: 'https://i.ibb.co/SDqNMqty/FZ2-Bj-Vl-Cl-Bd-Qt-FFpp-Kr-Z1fa-Rx-XSd-Soy-Dp-Cq-IAWw-I5-Dw-VVIE5n-Hj-K9-G7-L9p-M7-HUSs-Qc5-Yk-WMl-Ug.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-			'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg'
-		],
+    'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg',
+    'https://i.ibb.co/KzWN06nR/E8m-Bgd-ZWp-F4-OEW8x-FNd36x6h-AHSNw-UFE3-HNy2d2v-QQyb3-Ay-Yy6-E8uj-BCbr-KSLOp-VNQ2ea-Sg-Gg.jpg'
+],
                 description: 'Светлый номер на втором этаже с двуспальной кроватью. Отличный вид из окна. Душ и туалет на территории, кухонная зона в беседке.'
             },
             {
@@ -120,8 +118,8 @@ function getDefaultData() {
                 price: '2 400 ₽',
                 image: 'https://i.ibb.co/ymchdC6z/Qr-DVq-Cd-CXeo-Ti-E1-W0-MH2-Rs-QLMCy-W6k-KV7-Fh-D6fc-I2u-Dk-FOvgf-Rw-BQp-Fa-XVL5-Fi-G1-O5-Ot8-MC2s-A.jpg', // ← СЮДА ССЫЛКУ
                 gallery: [
-			'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg'
-		],
+    'https://i.ibb.co/XZMqd5RW/iu8b-YO0-ABF5-Eh-VXo2ka0-ORQyy-Pm1-Hn-Gm-YOuz-Az-Cm4k-Jl-Bx-Jn-FYAhqi-Qh-GM2g-QNOho-Qw-9d31l-C0ka-Cw.jpg'
+],
                 description: 'Комфортный номер на втором этаже с двумя односпальными кроватями. Душ и туалет на территории, кухонная зона в беседке.'
             }
         ]
